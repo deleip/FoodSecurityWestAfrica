b1d424e7fe5185f0bbd4ef2b023f4ccec79ef00e
LEIP Debbora <leip@iiasa.ac.at>
2020-12-29 12:29:02 +0100

    Testing commit on server

--- UpdatedModel/FunctionsStoOpt.py
+++ UpdatedModel/FunctionsStoOpt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+﻿#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Wed Nov 11 23:31:27 2020
@@ -21,7 +21,7 @@ import sys
 import matplotlib.pyplot as plt
 import math
 import os
-# from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.basemap import Basemap
 import matplotlib.cm as cm
 import matplotlib.colors as col
 from termcolor import colored
